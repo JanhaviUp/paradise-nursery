35e9a72c77fb67b669773ddb6858bd09f7d3e12e
JanhaviUp <124653735+JanhaviUp@users.noreply.github.com>
2026-03-23 13:55:22 +0530

    Add AboutUs component for company information

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>We are a company that provides beautiful plants and gardening solutions.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
